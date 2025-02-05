b9ac6868d7befa865f8517ef83bf0762abb25ab4
Midhun Sankar <midhunsankarprotonmail@gmail.com>
2025-02-05 14:28:21 +0530

    Small

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:pharma/screens/HomeScreen.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: HomeScreen(),
+    );
   }
 }
+
